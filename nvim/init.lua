e70bd65e168ef7505b5e003c6c27a83f4013e86e
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-05-15 22:53:00 +0100

    rearraging plugins
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,20 @@
 -- https://martinlwx.github.io/en/config-neovim-from-scratch
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require('options')    
 require('keymaps')
-require('plugins')        
+-- require('plugins')        
+require("lazy").setup("plugins")    
 require('colorscheme')
 require('lsp')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,17 +1,6 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
+--Any lua file in ~/.config/nvim/lua/plugins/*.lua 
+--  will be automatically merged in the main plugin
+return {
     "tanvirtin/monokai.nvim",
     -- Vscode-like pictograms
     {
@@ -93,50 +82,5 @@ require("lazy").setup({
 
         },
     },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        keys = {
-            {
-                "<leader>fe",
-                function()
-                    require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-                    --require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
-                end,
-                desc = "Explorer NeoTree (Root Dir)",
-            },
-            {
-                "<leader>fE",
-                function()
-                    require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-                end,
-                desc = "Explorer NeoTree (cwd)",
-            },
-            { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-            { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-            {
-                "<leader>ge",
-                function()
-                    require("neo-tree.command").execute({ source = "git_status", toggle = true })
-                end,
-                desc = "Git Explorer",
-            },
-            {
-                "<leader>be",
-                function()
-                    require("neo-tree.command").execute({ source = "buffers", toggle = true })
-                end,
-                desc = "Buffer Explorer",
-            },
-        },
-    },
-   -- {"github/copilot.vim"},
-
-})
+}
 
